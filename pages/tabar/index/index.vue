b4122f6db383f764dac275b257a413626268b6ae
haha <136605477@qq.com>
2021-05-24 20:03:12 +0800

    update

---
+++ pages/tabar/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+		<navbar></navbar>
+		<view v-for="item in 100">
+			{{item}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello123'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+
